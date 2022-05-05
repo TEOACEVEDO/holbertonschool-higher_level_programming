72912cb6b4c1bd5d02a552b4e501af8243d9c2fe
TEOACEVEDO <TEOACEVEDO.MA@gmail.com>
2022-05-05 12:26:01 -0500

    lo

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
-argv_1 = len(argv) - 1
-if(argv_1 == 0):
-    print("0 arguments.")
-elif(argv_1 == 1):
-    print(f"{argv_1} argument:")
-    print(f"{argv_1}: {argv[1]}")
-elif(argv_1 > 1):
-    print(f"{argv_1} argument:")
+if __name__ == "__main__":
+    from sys import argv
+    argv_1 = len(argv) - 1
+    if(argv_1 == 0):
+        print("0 arguments.")
+    elif(argv_1 == 1):
+        print(f"{argv_1} argument:")
+        print(f"{argv_1}: {argv[1]}")
+    elif(argv_1 > 1):
+        print(f"{argv_1} argument:")
     for i in range(1, argv_1 + 1):
         print(f"{i}: {argv[i]}")
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
-t = 1
-suma = 1
+if __name__ == "__main__":
+    from sys import argv
+    t, suma = 1, 0
 
-while t < len(argv):
-    suma += int(argv[t])
-    t += 1
+    while t < len(argv):
+        suma += int(argv[t])
+        t += 1
 
-print(suma)
+    print(suma)
