d467fb93e35e7a4566b14833cea5f990efe7ff6e
TEOACEVEDO <TEOACEVEDO.MA@gmail.com>
2022-05-09 12:35:45 -0500

    copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# in this fuction I used .copy() to copy a list 
+# in this fuction I used .copy() to copy a list
 def new_in_list(my_list, idx, element):
     list_og = my_list.copy()
     if idx < 0 or idx > len(my_list):
