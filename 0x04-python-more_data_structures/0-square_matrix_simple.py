0c7256c0f3f737ad52f8c86ecb55c08d8d0526f7
TEOACEVEDO <TEOACEVEDO.MA@gmail.com>
2022-05-10 12:47:52 -0500

    1247

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix_2 = matrix[:].copy()
-    for i in range(len(matrix_2)):
-        for x in range(len(matrix_2[i])):
-            matrix_2[i][x] = (matrix_2[i][x])**2
-    return matrix_2
+    return [list(map(lambda x: x ** 2, b)) for b in matrix]
