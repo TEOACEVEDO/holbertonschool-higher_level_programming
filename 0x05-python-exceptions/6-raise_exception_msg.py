d71e5e342408cec76717a310f306d77eab43bf18
TEOACEVEDO <TEOACEVEDO.MA@gmail.com>
2022-05-23 13:31:03 -0500

    name error

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-    a = 7
-    if not type(a) is str:
-        raise NameError()
+    if not type(message) is int:
+        raise NameError(message)
