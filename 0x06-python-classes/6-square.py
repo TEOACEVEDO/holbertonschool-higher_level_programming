f1dc49e15e7c4cf4217c0179b6129ae7e2eb7b03
TEOACEVEDO <TEOACEVEDO.MA@gmail.com>
2022-05-24 12:59:44 -0500

    square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        self.__position = value
+        if not type(value) == tuple:
+            self.__position = None
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) is not int or type(value[1]):
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError('size must be >= 0')
+        else:
+            raise TypeError('size must be an integer')
+    
+    def area(self):
+        return self.__size ** 2
+    
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for a in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                print("{}{}".format(" " * self.position[0], "#" * self.__size))
