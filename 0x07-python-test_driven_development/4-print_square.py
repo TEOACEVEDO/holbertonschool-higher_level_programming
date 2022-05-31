63e66d49e9cc104391b8da9853e36608cb3db78e
TEOACEVEDO <TEOACEVEDO.MA@gmail.com>
2022-05-31 08:34:43 -0500

    coments

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,5 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/python3}
+"""
+Function that prints a square
+"""
+
+
 def print_square(size):
+    """
+    Function that prints a square
+    """
+    
     if not type(size) is (int):
         raise TypeError('size must be an integer')
     if size < 0:
