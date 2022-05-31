6b3fb9b346f2236a1435c541c2a572b57781200d
TEOACEVEDO <TEOACEVEDO.MA@gmail.com>
2022-05-31 08:28:58 -0500

    test

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -2,7 +2,7 @@
 def text_indentation(text):
     if not type(text) is (str):
         raise TypeError("text must be a string")
-    
+
     for i in text:
         if i in ['.', '?', ':']:
             print(i, end='')
