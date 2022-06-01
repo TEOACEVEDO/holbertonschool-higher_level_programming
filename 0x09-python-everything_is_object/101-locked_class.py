b139f4b52aeb9b617125a915930d505f40a895c2
TEOACEVEDO <TEOACEVEDO.MA@gmail.com>
2022-06-01 18:52:52 -0500

    6:50

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-class LockedClass: __slots__ = 'first_name'
+class LockedClass:
+    """We use __slots__ to prevent the user from dynamically creating new instance attributes"""
+    __slots__ = 'first_name'
