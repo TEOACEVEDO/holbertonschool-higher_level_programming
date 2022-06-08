af732f3bd96f74d112b918c00cf9e3a056013a04
TEOACEVEDO <TEOACEVEDO.MA@gmail.com>
2022-06-08 18:14:45 -0500

    pascal

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 def read_file(filename=""):
     """this function is used for open file"""
 
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         read_data = f.read()
         print(read_data)
