e7814a2fd15292a1cc5d9150e9d2e27490d71664
TEOACEVEDO <TEOACEVEDO.MA@gmail.com>
2022-06-08 18:17:05 -0500

    pascal

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -3,18 +3,19 @@
 
 
 class Student:
-    """Constructor"""
-    def __init__(self, first_name, last_name, age):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+        """Class"""
+        def __init__(self, first_name, last_name, age):
+                """Constructor"""
+                self.first_name = first_name
+                self.last_name = last_name
+                self.age = age
 
-    def to_json(self, attrs=None):
-        if attrs is None or type(attrs) is not list():
-            return self.__dict__
-        else:
+        def to_json(self, wi=None):
+            """Method Returns: [type]: dict"""
+            if wi is None:
+                return self.__dict__
             dict = {}
-            for i in attrs:
+            for i in wi:
                 if i in self.__dict__:
                     dict[i] = self.__dict__[i]
             return dict
