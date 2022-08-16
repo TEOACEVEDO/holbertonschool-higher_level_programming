d169cb765d81d2a48c684c2b33ccb2f2cbcfa960
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-08-15 22:16:50 -0500

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-let a = parseInt(process.argv[2]);
-let b = parseInt(process.argv[3]);
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
 
 const Result = a + b;
-console.log(Result)
+console.log(Result);
