7679ee4667746dcd06e19483c5e217192cf62aab
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-08-15 21:08:51 -0500

    for

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let array = ['C is fun', 'Python is cool', 'JavaScript is amazing']
 
-for (i = 0; i <= 2; i++) {
-console.log(array[i]);
+for (let i = 0; i <= 2; i++) {
+  console.log(array[i]);
 }
