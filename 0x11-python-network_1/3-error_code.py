61a4fa8ce1351b62906b9b6e45ae7128d1601e94
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-09-02 20:28:44 -0500

    3error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 """
+Python script non-importable
 """
 
 
+from sys import argv
 import urllib.error
 import urllib.request
-from sys import argv
 
 if __name__ == "__main__":
     """
+    script that takes in a URL, sends a request to
+    the URL and displays the body of the response.
     """
-    try:
-        with urllib.request.urlopen(argv[1]) as response:
-            html = response.read()
-            print(html.decode("utf-8"))
 
-    except urllib.error.HTTPError as errr:
-        print("Error code: {}".format(errr.code))
+    url = argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as res:
+            html = res.read()
+            print(html.decode('utf-8'))
+    except urllib.error.HTTPError as err:
+        print("Error code: {}".format(err.code))
