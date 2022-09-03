aa75c00270652b1d2e9e3168a9b6a47eed7f6bf9
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-09-02 20:33:32 -0500

    6postemail

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 """
+Python script
 """
 
 
-import requests
 from sys import argv
+import requests
 
 if __name__ == "__main__":
     """
+    Script that takes in a URL and an email address, sends a
+    POST request to the passed URL with the email as a parameter,
+    and finally displays the body of the response.
     """
-    values = {"email": argv[2]}
-    r = requests.post(argv[1], values)
-    print(r.text)
+
+    url = argv[1]
+    value = {"email": argv[2]}
+
+    req = requests.post(url, data=value)
+
+    print(req.text)
