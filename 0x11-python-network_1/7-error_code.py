49a9160ad02cd63001cca7c3a08c92a168e56079
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-09-02 20:35:10 -0500

    8-api

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+Python script
 """
 
 
@@ -8,9 +9,14 @@ from sys import argv
 
 if __name__ == "__main__":
     """
+    Script that takes in a URL, sends a request to the URL
+    and displays the body of the response.
     """
-    r = requests.get(argv[1])
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
+
+    url = argv[1]
+
+    html = requests.get(argv[1])
+    if html.status_code >= 400:
+        print("Error code:", html.status_code)
     else:
-        print(r.text)
+        print(html.text)
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+Python script
 """
 
 
@@ -8,19 +9,24 @@ from sys import argv
 
 if __name__ == "__main__":
     """
+    Script that takes in a letter and sends a POST request to
+    http://0.0.0.0:5000/search_user with the letter as a parameter.
     """
+
     if len(argv) == 1:
-        q = ""
+        q = ''
     else:
         q = argv[1]
 
-    r = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
-
     try:
-        dato = r.json()
-        if dato == {}:
+        url = "http://0.0.0.0:5000/search_user"
+        value = {'q': q}
+
+        html = requests.post(url, value).json()
+
+        if len(html) == 0:
             print("No result")
         else:
-            print("[{}] {}".format(dato.get("id"), dato.get("name")))
+            print("[{}] {}".format(html.get("id"), html.get("name")))
     except ValueError:
         print("Not a valid JSON")
