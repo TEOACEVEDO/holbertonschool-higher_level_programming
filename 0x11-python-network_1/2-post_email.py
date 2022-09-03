2cdeb1ff770b6790b672c24322fb833a9ca01a3a
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-09-02 20:10:52 -0500

    2email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 """
+Python script
 """
 
 
-import urllib.parse
-import urllib.request
 from sys import argv
+from urllib import request, parse
 
 if __name__ == "__main__":
     """
+    script that takes in a URL and an email, sends a POST
+    request to the passed URL with the email as a parameter,
+    and displays the body of the response (decoded in utf-8)
     """
-    values = {"email": argv[2]}
-    data = urllib.parse.urlencode(values)
-    data = data.encode("ascii")
-    req = urllib.request.Request(argv[1], data)
-    with urllib.request.urlopen(req) as response:
-        html = response.read()
-        print(html.decode("utf-8"))
+
+    url = argv[1]
+    mail = argv[2]
+
+    data = parse.urlencode({"email": mail}).encode('ascii')
+
+    with request.urlopen(url, data) as res:
+        html = res.read()
+        print(html.decode('utf-8'))
