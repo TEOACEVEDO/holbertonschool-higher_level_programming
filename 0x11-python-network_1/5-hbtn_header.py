7e4886f4e8d68c173e225b4321299001a2ad94c9
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-09-02 20:32:03 -0500

    5header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 """
+Python script
 """
 
 
-import requests
 from sys import argv
+import requests
 
 if __name__ == "__main__":
     """
+    Script that takes in a URL, sends a request to the URL
+    and displays the value of the variable X-Request-Id in
+    the response header
     """
-    html = requests.get(argv[1])
-    print((html.headers).get("X-Request-Id"))
+    url = argv[1]
+
+    html = requests.get(url)
+    print(html.headers.get('X-Request-Id'))
